98605a0577b26d111badd5d25f093d3302736123
djonko <prevostdjonko@gmail.com>
2023-12-24 19:55:32 -0500

    refactoring

--- increase_disk.sh
+++ increase_disk.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 declare -A SERVERS
-SERVERS[556]=+38G
 SERVERS[551]=+38G
 SERVERS[552]=+38G
 SERVERS[553]=+38G
 SERVERS[554]=+38G
+SERVERS[555]=+38G
 for key in "${!SERVERS[@]}"
 do
+  qm stop "$key"
 	qm resize "$key" scsi0 "${SERVERS[$key]}"
+	qm start "$key"
 done
